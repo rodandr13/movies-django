ac992332156c04eb0ac62c41ba76652697d7f6dd
andrey <rodandr13@gmail.com>
2023-03-01 15:52:42 +0300

    refactoring: edit view

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,25 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, DetailView
 
 from .models import Movie
 
 
-class MovieView(View):
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(
-            request,
-            "movies/movies.html",
-            {"movie_list": movies}
-        )
+class MovieView(ListView):
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = "movies/movies.html"
 
 
-class MovieDetailView(View):
-
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(
-            request,
-            "movies/movie_detail.html",
-            {"movie": movie}
-        )
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = "url"
