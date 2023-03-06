d8c10a2fb89344e57430a881ea984357486cf01a
andrey <rodandr13@gmail.com>
2023-03-06 21:04:58 +0300

    feat: add inclusion tag

--- backend/movies/templatetags/movie_tag.py
+++ backend/movies/templatetags/movie_tag.py
@@ -1,5 +1,5 @@
 from django import template
-from ..models import Category
+from ..models import Category, Movie
 
 
 register = template.Library()
@@ -8,3 +8,9 @@ register = template.Library()
 @register.simple_tag()
 def get_categories():
     return Category.objects.all()
+
+
+@register.inclusion_tag("movies/tags/last_movies.html")
+def get_last_movies(count=5):
+    movies = Movie.objects.order_by("id")[:count]
+    return {"last_movies": movies}
