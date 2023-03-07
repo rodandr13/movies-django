b6fdd2bbb94c54a86065d67bd7ac66f6fd45403b
andrey <rodandr13@gmail.com>
2023-03-07 22:22:38 +0300

    feat: add filters

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.MovieView.as_view()),
+    path("filter/", views.FilterMoviesView.as_view(), name="filter"),
     path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
     path("review/<int:pk>/", views.AddReview.as_view(), name="add_review"),
     path("actor/<slug:slug>/", views.ActorView.as_view(), name="actor_detail"),
--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,17 +1,27 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 
-from .models import Movie, Category, Actor
+from .models import Movie, Category, Actor, Genre
 from .forms import ReviewForm
 
 
-class MovieView(ListView):
+class GenreYear:
+
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MovieView(GenreYear, ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     model = Movie
     slug_field = "url"
 
@@ -30,7 +40,17 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     model = Actor
     template_name = "movies/actor.html"
     slug_field = "name"
+
+
+class FilterMoviesView(GenreYear, ListView):
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre"))
+        )
+        return queryset
