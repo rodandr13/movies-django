9635773680c917cba62a451d20a69b9b8a72b44a
andrey <rodandr13@gmail.com>
2023-03-07 23:04:59 +0300

    feat: add rating

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -129,7 +129,7 @@ class RatingStar(models.Model):
     value = models.PositiveSmallIntegerField("Значение", default=0)
 
     def __str__(self):
-        return self.value
+        return f"{self.value}"
 
     class Meta:
         verbose_name = "Звезда рейтинга"
