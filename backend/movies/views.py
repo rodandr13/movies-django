e5d580e5059843da2db18050abd9863bd8945359
andrey <rodandr13@gmail.com>
2023-03-08 21:56:46 +0300

    feat: add pagination

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -20,6 +20,7 @@ class GenreYear:
 class MovieView(GenreYear, ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
+    paginate_by = 1
 
 
 class MovieDetailView(GenreYear, DetailView):
@@ -53,13 +54,20 @@ class ActorView(GenreYear, DetailView):
 
 
 class FilterMoviesView(GenreYear, ListView):
+    paginate_by = 2
 
     def get_queryset(self):
         queryset = Movie.objects.filter(
             Q(year__in=self.request.GET.getlist("year")) |
             Q(genres__in=self.request.GET.getlist("genre"))
-        )
+        ).distinct()
         return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["year"] = ''.join([f"year={x}&" for x in self.request.GET.getlist("year")])
+        context["genre"] = ''.join([f"genre={x}&" for x in self.request.GET.getlist("genre")])
+        return context
 
 
 class AddStarRating(View):
