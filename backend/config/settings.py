1b305a8082cd172fe4b0e82db3fd62e0dc3e3002
andrey <rodandr13@gmail.com>
2023-03-10 23:06:55 +0300

    feat: add locale in config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -125,6 +125,10 @@ LANGUAGES = (
     ("en", get_text("English")),
 )
 
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, "locale"),
+)
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
